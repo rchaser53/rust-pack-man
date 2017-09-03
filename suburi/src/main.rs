7f906aa084e7fe2b0bfdffc8fd6597c12d283456
rchaser53 <tayoshizawa29@gmail.com>
2017-09-03 23:08:44 +0900

    actually almost the same code as the sample

--- suburi/src/main.rs
+++ suburi/src/main.rs
@@ -1,19 +1,66 @@
-struct Nyan {
-}
+extern crate sdl2;
 
-impl Nyan {
-    fn aon() {
-        println!("aon")
-    }
-
-    fn gyan(self) {
-        println!("gyan")
-    }
-}
+use std::process;
+use sdl2::rect::{Rect};
+use sdl2::event::{Event};
+use sdl2::keyboard::Keycode;
 
 fn main() {
-    println!("gy-n");
-    Nyan::aon();
-    let nyan: Nyan = Nyan{};
-    nyan.gyan();
+    let ctx = sdl2::init().unwrap();
+    let video_ctx = ctx.video().unwrap();
+
+    let window  = match video_ctx
+        .window("rust_to_js", 640, 640)
+        .position_centered()
+        .opengl()
+        .build() {
+            Ok(window) => window,
+            Err(err)   => panic!("failed to create window: {}", err)
+        };
+
+    let mut renderer = match window
+        .renderer()
+        .build() {
+            Ok(renderer) => renderer,
+            Err(err) => panic!("failed to create renderer: {}", err)
+        };
+
+    let mut rect = Rect::new(10, 10, 10, 10);
+
+    let black = sdl2::pixels::Color::RGB(0, 0, 0);
+    let white = sdl2::pixels::Color::RGB(255, 255, 255);
+
+    let mut events = ctx.event_pump().unwrap();
+
+    let mut main_loop = || {
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit {..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
+                    println!("nya-n");
+                    process::exit(1);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Left), ..} => {
+                    rect.x -= 10;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right), ..} => {
+                    rect.x += 10;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Up), ..} => {
+                    rect.y -= 10;
+                },
+                Event::KeyDown { keycode: Some(Keycode::Down), ..} => {
+                    rect.y += 10;
+                },
+                _ => {}
+            }
+        }
+
+        let _ = renderer.set_draw_color(black);
+        let _ = renderer.clear();
+        let _ = renderer.set_draw_color(white);
+        let _ = renderer.fill_rect(rect);
+        let _ = renderer.present();
+    };
+
+    loop { main_loop(); }
 }
