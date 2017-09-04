39282cd6ddcad88c0c5e94c454832baa7d1904f5
rchaser53 <tayoshizawa29@gmail.com>
2017-09-04 23:56:31 +0900

    no emit the circle

--- suburi/src/main.rs
+++ suburi/src/main.rs
@@ -5,6 +5,7 @@ use sdl2::video;
 use sdl2::rect::{Rect};
 use sdl2::event::{Event};
 use sdl2::keyboard::Keycode;
+use sdl2::gfx::primitives::DrawRenderer;
 
 pub mod messagebox;
 use messagebox::showMessage;
@@ -38,6 +39,8 @@ fn main() {
     let white = sdl2::pixels::Color::RGB(255, 255, 255);
 
     let mut events = ctx.event_pump().unwrap();
+
+    let mut circle = renderer.circle(100, 200, 50, black);
 
     let mut main_loop = || {
         for event in events.poll_iter() {
