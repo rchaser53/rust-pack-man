0846e614b7f9150051a4f6e0cb42275e2491afbc
rchaser53 <tayoshizawa29@gmail.com>
2017-09-12 22:26:27 +0900

    sounds wav file but just one time

--- suburi/src/main.rs
+++ suburi/src/main.rs
@@ -1,7 +1,9 @@
 extern crate sdl2;
 
-use std::{thread, time};
-use std::process;
+use std::{thread, process, time};
+use std::path::{PathBuf, Path};
+use std::borrow::Cow;
+
 use sdl2::video;
 use sdl2::rect::{Rect};
 use sdl2::event::{Event};
@@ -10,12 +12,59 @@ use sdl2::gfx::primitives::DrawRenderer;
 
 use sdl2::image::{LoadTexture, INIT_PNG, INIT_JPG};
 
-pub mod messagebox;
-use messagebox::showMessage;
+use sdl2::audio::{AudioCallback, AudioSpecDesired, AudioSpecWAV, AudioCVT};
 
 pub mod circle;
-use circle::CirclePosition;
-use circle::Direction;
+use circle::{CirclePosition, Direction};
+
+struct Sound {
+    data: Vec<u8>,
+    volume: f32,
+    pos: usize,
+}
+
+impl AudioCallback for Sound {
+    type Channel = u8;
+
+    fn callback(&mut self, out: &mut [u8]) {
+        for dst in out.iter_mut() {
+            *dst = (*self.data.get(self.pos).unwrap_or(&0) as f32 * self.volume) as u8;
+            self.pos += 1;
+        }
+    }
+}
+
+fn createDeviceMusic(audio_subsystem: &sdl2::AudioSubsystem) -> sdl2::audio::AudioDevice<Sound> {
+    let wav_file : Cow<'static, Path> = match std::env::args().nth(1) {
+        None => Cow::from(Path::new("./sine.wav")),
+        Some(s) => Cow::from(PathBuf::from(s))
+    };
+
+    let desired_spec = AudioSpecDesired {
+        freq: Some(44100),
+        channels: Some(1), // mono
+        samples: None      // default
+    };
+
+    return audio_subsystem.open_playback(None, &desired_spec, |spec| {
+        let wav = AudioSpecWAV::load_wav(wav_file)
+            .expect("Could not load test WAV file");
+
+        let cvt = AudioCVT::new(
+                wav.format, wav.channels, wav.freq,
+                spec.format, spec.channels, spec.freq)
+            .expect("Could not convert WAV file");
+
+        let data = cvt.convert(wav.buffer().to_vec());
+
+        // initialize the audio callback
+        Sound {
+            data: data,
+            volume: 0.25,
+            pos: 0,
+        }
+    }).unwrap();
+}
 
 fn createWindow(video_ctx: sdl2::VideoSubsystem , width: u32, height: u32) -> video::Window {
     return video_ctx
@@ -29,6 +78,7 @@ fn createWindow(video_ctx: sdl2::VideoSubsystem , width: u32, height: u32) -> vi
 fn main() {
     let ctx = sdl2::init().unwrap();
     let video_ctx = ctx.video().unwrap();
+    let audio_subsystem = ctx.audio().unwrap();
     let _image_context = sdl2::image::init(INIT_PNG | INIT_JPG).unwrap();
 
     let window = createWindow(video_ctx, 640, 640);
@@ -36,16 +86,16 @@ fn main() {
     let mut canvas = window.into_canvas().software().build().unwrap();
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("./hoge.jpg").unwrap();
-
     let black = sdl2::pixels::Color::RGB(0, 0, 0);
     let white = sdl2::pixels::Color::RGB(255, 255, 255);
-
     let mut events = ctx.event_pump().unwrap();
 
     let mut circlePosition = CirclePosition{
         x: 300, y:200, direction: Direction::east as i16, radius: 30,
         color: white, isOpeningMouth: true
     };
+
+    let device = createDeviceMusic(&audio_subsystem);
 
     let fifty_millis = time::Duration::from_millis(50);
     let mut main_loop = || {
@@ -60,6 +110,7 @@ fn main() {
                 Event::KeyDown { keycode: Some(Keycode::Left), ..} => {
                     circlePosition.x -= 10;
                     circlePosition.direction = Direction::west as i16;
+                    device.resume();
                 },
                 Event::KeyDown { keycode: Some(Keycode::Right), ..} => {
                     circlePosition.x += 10;
@@ -79,12 +130,7 @@ fn main() {
         canvas.set_draw_color(black);
         canvas.clear();
         canvas.set_draw_color(white);
-        // canvas.copy(&texture, None, None).expect("Render failed");
         circlePosition.movePosition(&canvas);
-
-        // canvas.copy_ex(&texture, None,
-        //                  Some(Rect::new(50, 50, 50, 50)), 30.0, None, false, false).unwrap();
-
         canvas.present();
     };
 
