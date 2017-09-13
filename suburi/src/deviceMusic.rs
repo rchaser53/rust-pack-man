469bfdce046ff0a22dfea47b30998c969083c7de
rchaser53 <tayoshizawa29@gmail.com>
2017-09-13 22:01:06 +0900

    extract createDeviceMusic from main.rs
    honestly i wanna use the path for Path::new

---
+++ suburi/src/deviceMusic.rs
@@ -0,0 +1,57 @@
+extern crate sdl2;
+
+use std;
+use std::borrow::Cow;
+use std::path::{PathBuf, Path};
+
+use sdl2::audio::{AudioCallback, AudioSpecDesired, AudioSpecWAV, AudioCVT};
+
+pub struct Sound {
+    data: Vec<u8>,
+    volume: f32,
+    pos: usize,
+}
+
+impl AudioCallback for Sound {
+    type Channel = u8;
+
+    fn callback(&mut self, out: &mut [u8]) {
+        for dst in out.iter_mut() {
+            *dst = (*self.data.get(self.pos).unwrap_or(&0) as f32 * self.volume) as u8;
+            self.pos += 1;
+        }
+        self.pos = 0;
+    }
+}
+
+pub fn createDeviceMusic(audio_subsystem: &sdl2::AudioSubsystem) -> sdl2::audio::AudioDevice<Sound> {
+    let wav_file: Cow<'static, Path> = match std::env::args().nth(1) {
+        None => Cow::from(Path::new("./sine.wav")),
+        Some(s) => Cow::from(PathBuf::from(s))
+    };
+
+    let desired_spec = AudioSpecDesired {
+        freq: Some(44100),
+        channels: Some(1), // mono
+        samples: None      // default
+    };
+
+    return audio_subsystem.open_playback(None, &desired_spec, |spec| {
+        let wav = AudioSpecWAV::load_wav(wav_file)
+            .expect("Could not load test WAV file");
+
+        let cvt = AudioCVT::new(
+                wav.format, wav.channels, wav.freq,
+                spec.format, spec.channels, spec.freq)
+            .expect("Could not convert WAV file");
+
+        let data = cvt.convert(wav.buffer().to_vec());
+
+        // initialize the audio callback
+        Sound {
+            data: data,
+            volume: 0.25,
+            pos: 0
+        }
+    }).unwrap();
+}
--- suburi/src/main.rs
+++ suburi/src/main.rs
@@ -1,71 +1,19 @@
 extern crate sdl2;
 
 use std::{thread, process, time};
-use std::path::{PathBuf, Path};
-use std::borrow::Cow;
 
 use sdl2::video;
 use sdl2::rect::{Rect};
 use sdl2::event::{Event};
 use sdl2::keyboard::Keycode;
 use sdl2::gfx::primitives::DrawRenderer;
-
 use sdl2::image::{LoadTexture, INIT_PNG, INIT_JPG};
 
-use sdl2::audio::{AudioCallback, AudioSpecDesired, AudioSpecWAV, AudioCVT};
+pub mod deviceMusic;
+use deviceMusic::createDeviceMusic;
 
 pub mod circle;
 use circle::{CirclePosition, Direction};
-
-struct Sound {
-    data: Vec<u8>,
-    volume: f32,
-    pos: usize,
-}
-
-impl AudioCallback for Sound {
-    type Channel = u8;
-
-    fn callback(&mut self, out: &mut [u8]) {
-        for dst in out.iter_mut() {
-            *dst = (*self.data.get(self.pos).unwrap_or(&0) as f32 * self.volume) as u8;
-            self.pos += 1;
-        }
-        self.pos = 0;
-    }
-}
-
-fn createDeviceMusic(audio_subsystem: &sdl2::AudioSubsystem, pos: usize) -> sdl2::audio::AudioDevice<Sound> {
-    let wav_file : Cow<'static, Path> = match std::env::args().nth(1) {
-        None => Cow::from(Path::new("./sine.wav")),
-        Some(s) => Cow::from(PathBuf::from(s))
-    };
-
-    let desired_spec = AudioSpecDesired {
-        freq: Some(44100),
-        channels: Some(1), // mono
-        samples: None      // default
-    };
-
-    return audio_subsystem.open_playback(None, &desired_spec, |spec| {
-        let wav = AudioSpecWAV::load_wav(wav_file)
-            .expect("Could not load test WAV file");
-
-        let cvt = AudioCVT::new(
-                wav.format, wav.channels, wav.freq,
-                spec.format, spec.channels, spec.freq)
-            .expect("Could not convert WAV file");
-
-        let data = cvt.convert(wav.buffer().to_vec());
-
-        // initialize the audio callback
-        Sound {
-            data: data,
-            volume: 0.25,
-            pos: 0
-        }
-    }).unwrap();
-}
 
 fn createWindow(video_ctx: sdl2::VideoSubsystem , width: u32, height: u32) -> video::Window {
     return video_ctx
@@ -96,6 +44,7 @@ fn main() {
         color: white, isOpeningMouth: true
     };
 
+    
     let mut device = createDeviceMusic(&audio_subsystem);
 
     let fifty_millis = time::Duration::from_millis(50);
