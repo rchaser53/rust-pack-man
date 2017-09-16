38105894920a40553cffe4809007cf52dcf4d101
rchaser53 <tayoshizawa29@gmail.com>
2017-09-16 20:51:59 +0900

    try to play the bgm only

--- music/src/main.rs
+++ music/src/main.rs
@@ -5,70 +5,75 @@ use std::path::Path;
 use sdl2::mixer::{  DEFAULT_CHANNELS, INIT_MP3, INIT_FLAC, INIT_MOD,
                     INIT_FLUIDSYNTH, INIT_MODPLUG, INIT_OGG, AUDIO_S16LSB};
 
-fn main() {
-    let music_file = Path::new("nyan.mp3");
-    let sound_file = Some(Path::new("poyo.mp3"));
-
-    let sdl = sdl2::init().unwrap();
-    let _audio = sdl.audio().unwrap();
-    let mut timer = sdl.timer().unwrap();
-    let _mixer_context = sdl2::mixer::init(INIT_MP3 | INIT_FLAC | INIT_MOD | INIT_FLUIDSYNTH |
-                                           INIT_MODPLUG | INIT_OGG)
-            .unwrap();
-
+fn setup_sdl2_mixier() {
     let frequency = 44100;
-    let format = AUDIO_S16LSB; // signed 16 bit samples, in little-endian byte order
-    let channels = DEFAULT_CHANNELS; // Stereo
+    let format = AUDIO_S16LSB;          // signed 16 bit samples, in little-endian byte order
+    let channels = DEFAULT_CHANNELS;    // Stereo
     let chunk_size = 1024;
+
     sdl2::mixer::open_audio(frequency, format, channels, chunk_size).unwrap();
 
     // Number of mixing channels available for sound effect `Chunk`s to play
     // simultaneously.
     sdl2::mixer::allocate_channels(4);
+}
 
-    {
-        let n = sdl2::mixer::get_chunk_decoders_number();
-        println!("available chunk(sample) decoders: {}", n);
-        for i in 0..n {
-            println!("  decoder {} => {}", i, sdl2::mixer::get_chunk_decoder(i));
-        }
-    }
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let _audio = sdl.audio().unwrap();
+    let mut timer = sdl.timer().unwrap();
+    let _mixer_context = sdl2::mixer::init(INIT_MP3 | INIT_FLAC | INIT_MOD | INIT_FLUIDSYNTH |
+                                           INIT_MODPLUG | INIT_OGG).unwrap();
+    setup_sdl2_mixier();
 
-    {
-        let n = sdl2::mixer::get_music_decoders_number();
-        println!("available music decoders: {}", n);
-        for i in 0..n {
-            println!("  decoder {} => {}", i, sdl2::mixer::get_music_decoder(i));
-        }
-    }
-
+    let music_file = Path::new("nyan.mp3");
     let music = sdl2::mixer::Music::from_file(music_file).unwrap();
 
-    music.play(1);
-
-    if let Some(sound_file_path) = sound_file {
-        let sound_chunk_res = sdl2::mixer::Chunk::from_file(sound_file_path);
-
-        match sound_chunk_res {
-            Ok(sound_chunk) => {
-                println!("chunk volume => {:?}", sound_chunk.get_volume());
-                println!("playing sound twice");
-                let play_res = sdl2::mixer::Channel::all().play(&sound_chunk, 1);
-
-                // This delay is needed because when the `Chunk` goes out of scope,
-                // the sound effect stops playing. Delay long enough to hear the
-                // sound.
-                timer.delay(5000);
-
-                match play_res {
-                    Ok(_) => println!("played sound"),
-                    Err(e) => println!("{:?}", e),
-                }
-            }
-            Err(e) => println!("Cannot load sound file: {:?}", e),
-        }
-    }
+    let _ = music.play(1);
+    timer.delay(5000);
 }
+
+    // let sound_file = Some(Path::new("poyo.mp3"));
+    // if let Some(sound_file_path) = sound_file {
+    //     let sound_chunk_res = sdl2::mixer::Chunk::from_file(sound_file_path);
+
+    //     match sound_chunk_res {
+    //         Ok(sound_chunk) => {
+    //             println!("chunk volume => {:?}", sound_chunk.get_volume());
+    //             println!("playing sound twice");
+    //             let play_res = sdl2::mixer::Channel::all().play(&sound_chunk, 1);
+
+    //             // This delay is needed because when the `Chunk` goes out of scope,
+    //             // the sound effect stops playing. Delay long enough to hear the
+    //             // sound.
+    //             timer.delay(5000);
+
+    //             match play_res {
+    //                 Ok(_) => println!("played sound"),
+    //                 Err(e) => println!("{:?}", e),
+    //             }
+    //         }
+    //         Err(e) => println!("Cannot load sound file: {:?}", e),
+    //     }
+    // }
+
+
+
+    // {
+    //     let n = sdl2::mixer::get_chunk_decoders_number();
+    //     println!("available chunk(sample) decoders: {}", n);
+    //     for i in 0..n {
+    //         println!("  decoder {} => {}", i, sdl2::mixer::get_chunk_decoder(i));
+    //     }
+    // }
+
+    // {
+    //     let n = sdl2::mixer::get_music_decoders_number();
+    //     println!("available music decoders: {}", n);
+    //     for i in 0..n {
+    //         println!("  decoder {} => {}", i, sdl2::mixer::get_music_decoder(i));
+    //     }
+    // }
 
 
 // println!("query spec => {:?}", sdl2::mixer::query_spec());
