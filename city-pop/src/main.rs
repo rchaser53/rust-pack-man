5c7f5333e306e992532e861c46e01c6453826a42
rchaser53 <tayoshizawa29@gmail.com>
2017-09-18 20:46:25 +0900

    try to use custom error class and extract it.

--- city-pop/src/main.rs
+++ city-pop/src/main.rs
@@ -10,113 +10,131 @@ use std::error::{Error};
 use std::fs::File;
 use std::io::prelude::*;
 
-impl fmt::Display for CliError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            CliError::Io(ref err) => err.fmt(f),
-            CliError::Csv(ref err) => err.fmt(f),
-            CliError::NotFound => write!(f, "No matching cities with a \
-                                             population were found."),
-        }
-    }
-}
+pub mod error_handling;
+use error_handling::{double_first, print_double_first};
 
-impl Error for CliError {
-    fn description(&self) -> &str {
-        match *self {
-            CliError::Io(ref err) => err.description(),
-            CliError::Csv(ref err) => err.description(),
-            CliError::NotFound => "not found",
-        }
-    }
-}
+// impl fmt::Display for CliError {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         match *self {
+//             CliError::Io(ref err) => err.fmt(f),
+//             CliError::Csv(ref err) => err.fmt(f),
+//             CliError::NotFound => write!(f, "No matching cities with a \
+//                                              population were found."),
+//         }
+//     }
+// }
 
-impl From<io::Error> for CliError {
-    fn from(err: io::Error) -> CliError {
-        CliError::Io(err)
-    }
-}
+// impl Error for CliError {
+//     fn description(&self) -> &str {
+//         match *self {
+//             CliError::Io(ref err) => err.description(),
+//             CliError::Csv(ref err) => err.description(),
+//             CliError::NotFound => "not found",
+//         }
+//     }
+// }
 
-impl From<csv::Error> for CliError {
-    fn from(err: csv::Error) -> CliError {
-        CliError::Csv(err)
-    }
-}
+// impl From<io::Error> for CliError {
+//     fn from(err: io::Error) -> CliError {
+//         CliError::Io(err)
+//     }
+// }
 
-#[derive(Debug)]
-enum CliError {
-    Io(io::Error),
-    Csv(csv::Error),
-    NotFound,
-}
+// impl From<csv::Error> for CliError {
+//     fn from(err: csv::Error) -> CliError {
+//         CliError::Csv(err)
+//     }
+// }
 
-#[derive(Debug, RustcDecodable)]
-struct Row {
-    country: String,
-    city: String,
-    accent_city: String,
-    region: String,
+// #[derive(Debug)]
+// enum CliError {
+//     Io(io::Error),
+//     Csv(csv::Error),
+//     NotFound,
+// }
 
-    population: Option<u64>,
-    latitude: Option<f64>,
-    longitude: Option<f64>,
-}
+// #[derive(Debug, RustcDecodable)]
+// struct Row {
+//     country: String,
+//     city: String,
+//     accent_city: String,
+//     region: String,
 
-struct PopulationCount {
-    city: String,
-    country: String,
-    count: u64,
-}
+//     population: Option<u64>,
+//     latitude: Option<f64>,
+//     longitude: Option<f64>,
+// }
 
-fn print_usage(program: &str, opts: Options) {
-    println!("{}", opts.usage(&format!("Usage: {} [options] <city>", program)));
-}
+// struct PopulationCount {
+//     city: String,
+//     country: String,
+//     count: u64,
+// }
 
-fn search<P: AsRef<Path>>
-         (file_path: &Option<P>, city: &str)
-         -> Result<Vec<PopulationCount>, CliError> {
-    let mut found = vec![];
-    let input: Box<io::Read> = match *file_path {
-        None => Box::new(io::stdin()),
-        Some(ref file_path) => Box::new(try!(fs::File::open(file_path))),
-    };
-    let mut rdr = csv::Reader::from_reader(input);
-    for row in rdr.decode::<Row>() {
-        let row = try!(row);
-        match row.population {
-            None => { } // スキップする
-            Some(count) => if row.city == city {
-                found.push(PopulationCount {
-                    city: row.city,
-                    country: row.country,
-                    count: count,
-                });
-            },
-        }
-    }
-    if found.is_empty() {
-        Err(CliError::NotFound)
-    } else {
-        Ok(found)
-    }
-}
+// fn print_usage(program: &str, opts: Options) {
+//     println!("{}", opts.usage(&format!("Usage: {} [options] <city>", program)));
+// }
 
-fn cat(path: &Path) -> Result<String, std::io::Error> {
-    let mut f = try!(File::open(path));
-    let mut s = String::new();
-    f.read_to_string(&mut s);
-    return Ok(s);
-}
+// fn search<P: AsRef<Path>>
+//          (file_path: &Option<P>, city: &str)
+//          -> Result<Vec<PopulationCount>, CliError> {
+//     let mut found = vec![];
+//     let input: Box<io::Read> = match *file_path {
+//         None => Box::new(io::stdin()),
+//         Some(ref file_path) => Box::new(try!(fs::File::open(file_path))),
+//     };
+//     let mut rdr = csv::Reader::from_reader(input);
+//     for row in rdr.decode::<Row>() {
+//         let row = try!(row);
+//         match row.population {
+//             None => { } // スキップする
+//             Some(count) => if row.city == city {
+//                 found.push(PopulationCount {
+//                     city: row.city,
+//                     country: row.country,
+//                     count: count,
+//                 });
+//             },
+//         }
+//     }
+//     if found.is_empty() {
+//         Err(CliError::NotFound)
+//     } else {
+//         Ok(found)
+//     }
+// }
 
 fn main() {
-    let ab = match cat(Path::new("test.csv")) {
-        Err(why) => {
-            println!("! {:?}", why.kind());
-            panic!("nya");
-        },
-        Ok(s) => s
-    };
-    println!("{}", ab);
+    let numbers = vec!["93", "18"];
+    let empty = vec![];
+    let strings = vec!["tofu", "93", "18"];
+
+    print_double_first(double_first(numbers));
+    print_double_first(double_first(empty));
+    print_double_first(double_first(strings));
+}
+
+// fn cat(path: &Path) -> Result<String, std::io::Error> {
+//     let mut f = try!(File::open(path));
+//     let mut s = String::new();
+//     f.read_to_string(&mut s);
+//     return Ok(s);
+// }
+
+// fn main() {
+//     let ab = match cat(Path::new("test.csv")) {
+//         Err(why) => {
+//             println!("! {:?}", why.kind());
+//             panic!("nya");
+//         },
+//         Ok(s) => s
+//     };
+//     println!("{}", ab);
+
+//     process::exit(0);
+// }
+
+
 
     // let args: Vec<String> = env::args().collect();
     // let program = args[0].clone();
@@ -180,9 +198,4 @@ fn main() {
         
         // println!("{}, {}: {:?}", pop.city, pop.country, pop.count);
     // }
-
-    // let hoge = search(Path::new("test.csv"), "city");
-
-
-    process::exit(0);
-}
+// }
