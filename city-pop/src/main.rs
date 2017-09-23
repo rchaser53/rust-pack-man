3d0b43d62494862f2e85439545c5bbec19d3f6a3
rchaser53 <tayoshizawa29@gmail.com>
2017-09-23 15:01:58 +0900

    try to use and_then, map and map_err

--- city-pop/src/main.rs
+++ city-pop/src/main.rs
@@ -1,10 +1,9 @@
-use std::{fs, env, io, fmt, process};
-use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
-use std::num::ParseIntError;
-
+// use std::{fs, env, io, fmt, process};
+// use std::error::Error;
+// use std::num::ParseIntError;
 
 fn extract_file_data(path: &Path) -> Result<String, std::io::Error> {
     let mut file = File::open(&path)?;
@@ -13,11 +12,25 @@ fn extract_file_data(path: &Path) -> Result<String, std::io::Error> {
     return Ok(s);
 }
 
-fn extractDataFromLine(a: i16) -> Result<i16, String> {
-    return match a {
-        a if a < 10 => Err(a.to_string()),
-        _ => Ok(a)
+fn is_below_ten(num: i16) -> Result<i16, String> {
+    return match num {
+        num if num < 10 => Err(num.to_string()),
+        _ => Ok(num)
     };
+}
+
+fn is_over_twenty(num: i16) -> Result<i16, String> {
+    return match num {
+        num if 20 < num => Err(num.to_string()),
+        _ => Ok(num)
+    };
+}
+
+fn extract_data_from_line(num: i16) -> Result<i16, String> {
+    return is_below_ten(num).and_then(is_over_twenty);
+    // return is_below_ten(num)
+    //             .map(|n| n)
+    //             .map_err(|err| err);
 }
 
 fn main() {
@@ -29,8 +42,7 @@ fn main() {
     let lines: Vec<&str> = csv.lines().collect();
     // let _vec: Vec<&str> = csv.split("\n").collect();
 
-    // let aaa: i16 = match extractDataFromLine(9) {
-    let aaa: i16 = match extractDataFromLine(10) {
+    let aaa: i16 = match extract_data_from_line(10) {
                         Err(why) => panic!("{} is less than 10.", why),
                         Ok(data) => data
                     };
