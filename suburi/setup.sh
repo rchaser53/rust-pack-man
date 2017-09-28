a66c0b33b4d8096928c499a3d7f5db8139859d4f
rchaser53 <t.yoshizawa@sorabito.com>
2017-09-28 17:06:58 +0900

    add cargo-edit for rust setup

--- suburi/setup.sh
+++ suburi/setup.sh
@@ -1,3 +1,4 @@
 curl https://sh.rustup.rs -sSf | sh
 brew install sdl2 sdl2_gfx sdl2_image sdl2_ttf
 brew install sdl2_mixer --with-flac --with-fluid-synth --with-libmikmod --with-libmodplug --with-libvorbis --with-smpeg2
+cargo install cargo-edit
