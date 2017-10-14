4bd2879b5986034951b3628e1a8411bf145a68e6
rchaser53 <tayoshizawa29@gmail.com>
2017-10-14 19:23:26 +0900

    implement 'orange color circle item'
    just only rendering, cannot touch it

--- src/game_field/cell_feature.rs
+++ src/game_field/cell_feature.rs
@@ -6,6 +6,7 @@ use game_field::field_cell::{CellStatus, CellType};
 use constants::{BackgroundColor};
 
 const ITEM_RADIUS: i16 = 10;
+const CELL_PADDING: i32 = 15;
 
 pub fn draw_myself(cell_status: &CellStatus, renderer: &mut render::Canvas<video::Window>) -> () {
     let color = convert_cell_type_to_background_color(cell_status.cell_type);
@@ -28,6 +29,8 @@ pub fn convert_cell_type_to_background_color(cell_type: CellType) -> pixels::Col
 pub struct Item {}
 impl Item {
     fn draw_unique_feature(cell_status: &CellStatus, renderer: &mut render::Canvas<video::Window>) -> () {
+        let _ = renderer.filled_circle((cell_status.x + CELL_PADDING) as i16,
+                  (cell_status.y + CELL_PADDING) as i16, ITEM_RADIUS, BackgroundColor::Yellow.value());
     }
 }
 
