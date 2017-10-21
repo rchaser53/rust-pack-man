3f90fdf5000ebed22980e7eda855a752f88819fc
rchaser53 <tayoshizawa29@gmail.com>
2017-10-21 23:35:10 +0900

    add files to run server for wasm

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+
+app.use('/', express.static('wasm'));
+app.use('/', express.static(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
+});
+
+server.listen(3000, () => {
+  console.log('run server');
+});
