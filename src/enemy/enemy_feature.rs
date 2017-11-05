04901fe00b96293b17a550a55fc96a49c959ca9f
rchaser53 <tayoshizawa29@gmail.com>
2017-11-05 17:39:12 +0900

    enemy moves randomly

--- src/enemy/enemy_feature.rs
+++ src/enemy/enemy_feature.rs
@@ -1,5 +1,7 @@
 use sdl2::gfx::primitives::DrawRenderer;
 use sdl2::{render, video};
+use rand;
+use rand::Rng;
 
 use enemy::enemy_status::{EnemyStatus};
 
@@ -14,7 +16,19 @@ pub trait EnemyAction {
   fn draw(&self, enemy_status: &EnemyStatus, renderer: &mut render::Canvas<video::Window>) {
     let _ = renderer.filled_ellipse(enemy_status.x, enemy_status.y, enemy_status.width, enemy_status.height, enemy_status.background_color);
   }
+
+  fn get_random_sign(&self) -> i16 {
+    if rand::thread_rng().gen::<bool>() {
+      return 1
+    }
+    -1
+  }
 }
 
 pub struct NormalFeature {}
-impl EnemyAction for NormalFeature {}
+impl EnemyAction for NormalFeature {
+  fn update(&self, enemy_status: &mut EnemyStatus) {
+    enemy_status.x += self.get_random_sign() * rand::thread_rng().gen_range(0, 4);
+    enemy_status.y += self.get_random_sign() * rand::thread_rng().gen_range(0, 4);
+  }
+}
